b5e5f11b036ea687dc45ba0747751e59d85ee396
imankamaludin10 <50430670+imankamaludin10@users.noreply.github.com>
2024-01-25 23:57:22 +0700

    hari ke 3/366

---
+++ 3_hero.js
@@ -0,0 +1,22 @@
+// function hero(bullets, dragons) {
+//   let result = false;
+
+//   if (bullets >= dragons * 2) {
+//     result = true;
+//   }
+//   return result;
+// }
+
+// ternari
+// function hero(bullets, dragons) {
+//     return (bullets >= dragons * 2) ? true : false ;
+// }
+
+// ternari truety / falsty
+// function hero(bullets, dragons) {
+//   return bullets >= dragons * 2;
+// }
+
+const hero = (bullets, dragons) => bullets >= dragons * 2;
+
+console.log(hero(100, 40));
